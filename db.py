2c743970acc06b4b4c649f80c809ce5005be0fce
xton13 <mike.cutshaw@trojans.dsu.edu>
2018-08-04 13:58:57 -0600

    Init

---
+++ db.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+import hashlib
+import configparser
+import sqlite3
+
+class info_database:
+
+    def __init__(self,config):
+        try:
+            Config = configparser.ConfigParser()
+            Config.read(config)
+            self.db = Config['Main']['Database']
+        except:
+            print("Config Error!")
+            exit()
+        try:    
+            self.connect()
+        except:
+            print("Database Error!")
+
+    def close(self):
+        self.conn.close()
+
+    def connect(self):
+        self.conn = sqlite3.connect(self.db)
+        self.cur = self.conn.cursor()
+
+    def execute(self,command):
+        self.connect()
+        self.cur.execute(command)
+        self.conn.commit()
+        text_return = self.cur.fetchall()
+        self.close()
+        return text_return
+
+    def executevar(self,command,operands):
+        self.connect()
+        self.cur.execute(command,operands)
+        self.conn.commit()
+        text_return = self.cur.fetchall()
+        self.close()
+        return text_return
+
+    def returntop(self,table,fields,sort_field):
+        querystr = "SELECT " + ''.join(fields) +" FROM "+ table + " ORDER BY " + sort_field + " DESC"
+        return self.execute(querystr)
---
+++ www_page.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3 
+from flask import Flask, request, Response 
+import configparser 
+from db import *
+
+config = configparser.ConfigParser() 
+config_path = '/home/jack/Documents/info_server/info_server.config'
+config.read(config_path) 
+idb = info_database(config_path) 
+
+def table_validate(table,config):
+    tables = config["Main"]["Tables"].split(',')
+    if table in tables:
+        return 0
+
+    return 1
+def return_fields(table,config):
+    return config[table]["Fields"].split(',')
+
+class response_server: 
+    def __init__(self): 
+        self.app = Flask(__name__) 
+        @self.app.route('/info/<sub_path>', methods=["GET"]) 
+        def data(sub_path): 
+            if table_validate(sub_path,config) == 1:
+                return Response("")
+            
+            fields = return_fields(sub_path,config)
+            data = idb.returntop(sub_path,fields,"date")
+            return Response(str(data[0][0]))
+ 
+serv = response_server() 
+app = serv.app
